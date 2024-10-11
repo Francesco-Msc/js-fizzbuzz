4b97db497204c25f63d960134e735b3b578664a4
Francesco-Msc <moscafrancesco1998@gmail.com>
2024-10-11 14:01:46 +0200

    added condition if the numbre is multiple of 5 and 3 print FizzBuzz

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
 // eseguo un ciclo in cui mi stampa tutti i numeri da 1 a 100
 
 for (let i = 1; i <= 100; i++){
-    console.log(i);
+    
+    // aggiungo una condizione per cui se il numero è multiplo di 3 e di 5 stampo fizz buzz
+    if ((i % 5 === 0) && (i % 3 === 0)){
+        console.log('FizzBuzz');
+    } else {
+        console.log(i);
+    }
 }
