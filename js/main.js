9e1b3547e88a28a2a81175f68202aa8a3441b399
Francesco-Msc <moscafrancesco1998@gmail.com>
2024-10-11 14:12:09 +0200

    added all other conditions

--- js/main.js
+++ js/main.js
@@ -2,9 +2,14 @@
 
 for (let i = 1; i <= 100; i++){
     
-    // aggiungo una condizione per cui se il numero è multiplo di 3 e di 5 stampo fizz buzz
-    if ((i % 5 === 0) && (i % 3 === 0)){
+    //aggiungo delle condizioni
+    
+    if ((i % 5 === 0) && (i % 3 === 0)){        //se il numero è multiplo di 3 e di 5 stampo FizzBuzz
         console.log('FizzBuzz');
+    } else if (i % 5 === 0){                    //se il numero è multiplo di 5 stampa Buzz
+        console.log('Buzz');
+    } else if (i % 3 === 0){
+        console.log('Fizz');                    //se il numero è multiplo di 3 stampa Fizz
     } else {
         console.log(i);
     }
